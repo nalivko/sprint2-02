e6a2b4bab40871ffc0165541465575a5056de748
Dmitriy <nalivko.dmitr@gmail.com>
2024-07-05 15:07:14 +0300

    fix auth controller

--- src/features/auth/controllers/loginController.ts
+++ src/features/auth/controllers/loginController.ts
@@ -2,9 +2,7 @@ import { Request, Response } from "express"
 import { authService } from "../services/authService"
 
 export const loginController = async (req: Request, res: Response) => {
-    // res.send(req.body.login + ' ' + req.body.password)
     const user = await authService.login(req.body.loginOrEmail, req.body.password)
-    // res.send(user)
 
     if (!user) {
         res.sendStatus(401)
--- src/features/auth/services/authService.ts
+++ src/features/auth/services/authService.ts
@@ -1,7 +1,18 @@
 import { usersRepository } from "../../users/usersRepository"
+import bcrypt from 'bcrypt'
 
 export const authService = {
-    async login(loginOrEmail: string, password: string) {
-        return await usersRepository.findUserByLoginOrEmail(loginOrEmail)
+    async login(loginOrEmail: string, password: string): Promise<boolean> {
+        return await this.checkCredentials(loginOrEmail, password)
+    },
+
+    async checkCredentials(loginOrEmail: string, password: string): Promise<boolean> {
+        const user =  await usersRepository.findUserByLoginOrEmail(loginOrEmail)
+
+        if (!user) return false
+
+        const isCorrect = await bcrypt.compare(password, user.passwordHash)
+
+        return isCorrect
     }
 }
