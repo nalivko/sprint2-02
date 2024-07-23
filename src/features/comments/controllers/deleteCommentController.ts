53aa66ef33dd572cdb1ec780cb16aae4a56ff513
Dmitriy <nalivko.dmitr@gmail.com>
2024-07-23 14:13:50 +0300

    fix deleteCommentController

--- src/features/comments/controllers/deleteCommentController.ts
+++ src/features/comments/controllers/deleteCommentController.ts
@@ -3,11 +3,7 @@ import { commentsRepository } from "../repositories/commentsRepository";
 import { commentService } from "../services/commentsService";
 
 export const deleteCommentController = async (req: Request, res: Response) => {
-    const isDeleted = await commentService.deleteComment(req.params.commmentId, req.user.userId)
+    const result = await commentService.deleteComment(req.params.commmentId, req.user.userId)
 
-    if (isDeleted) {
-        res.send(204)
-    } else {
-        res.send(404)
-    }
+    res.send(result.status)
 }
